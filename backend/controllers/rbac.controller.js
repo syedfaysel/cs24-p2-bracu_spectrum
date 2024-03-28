83fe39d9fd7388014b8f5e737648dca956634256
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-03-29 05:15:40 +0600

    rbac all routes done

--- backend/controllers/rbac.controller.js
+++ backend/controllers/rbac.controller.js
@@ -1,58 +1,111 @@
-const Role = require("../models/role.model");
-const Permission = require("../models/permission.model");
-const User = require("../models/user.model");
+const Role = require("../models/accessRole.model");
+const Permission = require("../models/accessPolicy.model");
 
 
-// add permission to db
-const addPermission = async (req, res) => {
+// create new access policy (permission) // ok
+const createPermission = async (req, res) => {
   try {
-    const permission = new Permission(req.body);
-    await permission.save();
+    const { policy_name, resource_slug, category_name } = req.body;
+
+    if (!resource_slug || !policy_name) {
+      return res.status(400).send({
+        message: "Resource slug and policy name cannot be empty",
+      });
+    }
+    const query = { resource_slug, policy_name };
+    if(category_name) query.category_name = category_name;
+    const permission = await Permission.create(query);
     res.status(201).send(permission);
   } catch (error) {
     res.status(500).send({
-      message: error.message || "Some error occurred while creating the Permission.",
+      message:
+        error.message || "Some error occurred while creating the Permission.",
     });
   }
 };
+
+// retrieve all access poilicies (permissions) // ok
 const getPermissions = async (req, res) => {
   try {
     const permissions = await Permission.find();
     res.status(200).send(permissions);
   } catch (error) {
     res.status(500).send({
-      message: error.message || "Some error occurred while retrieving permissions.",
+      message:
+        error.message || "Some error occurred while retrieving permissions.",
     });
   }
-}
+};
 
-// delete permission
+// delete permission // ok
 const deletePermission = async (req, res) => {
   try {
-    const permission = await Permission.findByIdAndDelete(req.params.id);
+    const permission = await Permission.findByIdAndDelete(req.body.policy_id);
     if (!permission) {
       return res.status(404).send({
-        message: `Cannot delete Permission with id=${req.params.id}. Maybe Permission was not found!`,
+        message: `Cannot delete Permission with id=${req.body.policy_id}. Maybe Permission was not found!`,
       });
     }
-    res.status(200).send({ message: "Permission was deleted successfully!" });
-  
+    res.status(200).send({
+      message: `${req.body.policy_id} Permission was deleted successfully!`,
+    });
   } catch (error) {
     res.status(500).send({
-      message: error.message || "Could not delete Permission with id=" + req.params.id,
+      message:
+        error.message ||
+        `Could not delete Permission with id ${req.body.policy_id}`,
     });
   }
-}
+};
 
+// Access Role (Role) controller // ok
+const getRoles = async (req, res) => {
+  try {
+    const roles = await Role.find().populate("permissions");
+    res.status(200).send(roles);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Some error occurred while retrieving roles.",
+    });
+  }
+};
 
-// Create and Save a new Role
+// Create Role // ok
 const createRole = async (req, res) => {
   try {
-    console.log(req.body);
-    const role = await Role.create({
-      title: req.body.title,
-    })
-    res.status(201).send(role);
+    const { role_name, role_rank, role_description, permissions } = req.body;
+    // Validate request
+    const query = {};
+
+    if (role_name) {
+      query.role_name = role_name;
+    } else {
+      return res.status(400).send({
+        message: "Role name cannot be empty",
+      });
+    }
+
+    const role = await Role.findOne({ role_name: role_name });
+
+    if (!!role) {
+      return res.status(400).send({
+        message: `Role with name ${role_name} already exists`,
+      });
+    }
+
+    if (role_rank) query.role_rank = role_rank;
+    if (role_description) query.role_description = role_description;
+
+    if (permissions?.length > 0) {
+      const allPermissions = await Permission.find();
+      const filteredPermissions = allPermissions.filter((permission) => {
+        return permissions.includes(permission._id);
+      });
+      query.permissions = filteredPermissions;
+    }
+
+    const newRole = await Role.create(query);
+    res.status(201).send(newRole);
   } catch (error) {
     res.status(500).send({
       message: error.message || "Some error occurred while creating the Role.",
@@ -60,20 +113,27 @@ const createRole = async (req, res) => {
   }
 };
 
-// get all roles
-const getRoles = async (req, res) => {
+// Delete Role // ok
+const deleteRole = async (req, res) => {
   try {
-    const roles = await Role.find().populate('permissions');
-    res.status(200).send(roles);
+    const role = await Role.findByIdAndDelete(req.body.roleId);
+    if (!role) {
+      return res.status(404).send({
+        message: `Cannot delete Role with id=${req.body.roleId}. Maybe Role was not found!`,
+      });
+    }
+    res.status(200).send({
+      message: `${req.body.roleId}Role was deleted successfully!`,
+    });
   } catch (error) {
     res.status(500).send({
-      message: error.message || "Some error occurred while retrieving roles.",
+      message:
+        error.message || `Could not delete Role with id ${req.body.roleId}`,
     });
   }
-}
+};
 
-// assign permission to rule
-
+// Assign/Remove permission to/from role // ok
 const assignPermissionToRole = async (req, res) => {
   try {
     const role = await Role.findById(req.params.roleId);
@@ -82,31 +142,67 @@ const assignPermissionToRole = async (req, res) => {
         message: `Role not found with id ${req.params.roleId}`,
       });
     }
-    // check if permission exists
-    const label = req.body.label; // permission label
-    const permission = await Permission.findOne({label: label});
-    if (!permission) {
-      return res.status(404).send({
-        message: `Permission not found with id ${label}`,
+
+    // Check if permission IDs are provided in the request body
+    if (!req.body.permissions || !Array.isArray(req.body.permissions)) {
+      return res.status(400).send({
+        message: "Invalid permissions provided",
       });
     }
-    role.permissions.push(permission._id);
+
+    // Retrieve permissions based on the provided IDs
+    const permissionIds = req.body.permissions;
+    const permissions = await Permission.find({ _id: { $in: permissionIds } });
+    if (!permissions.length) {
+      return res.status(404).send({
+        message: `No permissions found to assign to role ${req.params.roleId}`,
+      });
+    }
+
+    // Determine the operation type (add or remove)
+    const operationType = req.body.operationType || "add"; // Default to "add" if not specified
+
+    // Update role permissions based on the operation type
+    if (operationType === "add") {
+      // Find permissions that are not already assigned to the role
+      const newPermissions = permissions.filter(
+        (permission) => !role.permissions.includes(permission._id)
+      );
+      // Assign new permissions to the role
+      role.permissions.push(
+        ...newPermissions.map((permission) => permission._id)
+      );
+    } else if (operationType === "remove") {
+      // Remove permissions that are currently assigned to the role
+      role.permissions = role.permissions.filter(
+        (permission) => !permissionIds.includes(permission.toString())
+      );
+    }
+
+    // Save the updated role
     await role.save();
-    const data = await role.populate('permissions');
-    res.status(200).send(role);
+
+    res.status(200).send({
+      message: `Permissions ${
+        operationType === "add" ? "assigned to" : "removed from"
+      } role successfully`,
+      data: role,
+    });
   } catch (error) {
+    console.error("Error assigning/removing permission to/from role:", error);
     res.status(500).send({
-      message: error.message || "Some error occurred while assigning permission to role.",
+      message:
+        "Some error occurred while assigning/removing permission to/from role.",
     });
   }
-
-}
+};
 
 module.exports = {
-  addPermission,
+  createPermission,
   getPermissions,
   deletePermission,
   assignPermissionToRole,
   createRole,
+  deleteRole,
   getRoles,
-}
+};
--- backend/routes/rbac.route.js
+++ backend/routes/rbac.route.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const rbac = require('../controllers/rbac.controller.js');
+const router = require("express").Router();
+const rbac = require("../controllers/rbac.controller.js");
 
-// parent path : /rbac 
-router.get('/permissions', rbac.getPermissions);
-router.post('/permissions', rbac.addPermission);
-router.post('/roles', rbac.createRole);
+// parent path : /rbac
+router.get("/permissions", rbac.getPermissions);
+router.post("/permissions", rbac.createPermission);
+router.delete("/permissions", rbac.deletePermission);
 router.get('/roles', rbac.getRoles);
-router.put('/roles/:roleId/permissions', rbac.assignPermissionToRole);
-
-
+router.post("/roles", rbac.createRole);
+router.delete("/roles", rbac.deleteRole);
+router.put("/roles/:roleId/permissions", rbac.assignPermissionToRole);
 
 module.exports = router;
