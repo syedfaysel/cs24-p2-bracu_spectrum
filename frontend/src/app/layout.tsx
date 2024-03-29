157074d14818743e38e4b6112a5106cde6a609c7
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-03-29 17:47:51 +0600

    frontend initial setup

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "EcoSync | DNCC",
+  description: "Administrative system for Waste Collection and Management | DNCC",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col gap-4 items-center justify-center py-24">
+      <h1 className="text-3xl">Welcome to EcoSync</h1>
+      <p>Administration & Management | All in one place</p>
+    </main>
+  );
+}
